9c4d8e6b575fc5f8b2b7b1afcb918c5ad4401ab0
Tobias Koppers <tobias.koppers@googlemail.com>
2025-04-10 15:28:47 +0200

    use annotated stack traces for tree shaking too

--- turbopack/crates/turbopack-ecmascript/src/tree_shake/chunk_item.rs
+++ turbopack/crates/turbopack-ecmascript/src/tree_shake/chunk_item.rs
@@ -122,6 +122,11 @@ impl EcmascriptChunkItem for SideEffectsModuleChunkItem {
         let mut code = RopeBuilder::default();
         let mut has_top_level_await = false;
 
+        let annotated_stack_traces = *self
+            .chunking_context
+            .should_use_annotated_stack_traces()
+            .await?;
+
         let module = self.module.await?;
 
         for &side_effect in self.module.await?.side_effects.iter() {
@@ -178,6 +183,7 @@ impl EcmascriptChunkItem for SideEffectsModuleChunkItem {
                 } else {
                     None
                 },
+                annotated_stack_traces,
                 ..Default::default()
             },
             additional_ids: Default::default(),
