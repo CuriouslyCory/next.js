22a71769b02051e0061c4ae3e635c28ce8b98ccd
Janka Uryga <lolzatu2@gmail.com>
2025-06-12 13:09:28 +0200

    scaffold root-params, add getRootParam for generated code

--- packages/next/src/server/config-shared.ts
+++ packages/next/src/server/config-shared.ts
@@ -778,6 +778,11 @@ export interface ExperimentalConfig {
    * @default false
    */
   optimizeRouterScrolling?: boolean
+
+  /**
+   * Enable accessing root params via the `next/root-params` module.
+   */
+  rootParams?: boolean
 }
 
 export type ExportPathMap = {
